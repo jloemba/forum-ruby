31a10305a5a752c537bef024481408c20a175b88
Joris <jorisloemba@gmail.com>
2019-07-17 10:47:52 +0200

    Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+Users.destroy_all
+#Article.create(title: 'Bienvenue sur Ruby on Rails',content: 'Learning to build 
+#a modern web application is daunting. Ruby on Rails makes it much easier 
+#and more fun. It includes everything you need to build 
+#fantastic applications, 
+#and you can learn it with the support 
+#of our large, friendly community.',slug: 'bienvenue-sur-ruby-on-rails')
+
+#Article.create(title: 'Projet Ruby x ReactJS',content: 'Une bibliothèque JavaScript pour créer 
+#des interfaces utilisateurs',slug: 'projet-ruby-x-reactjs')
+
+#Article.create(title: 'Hello world',content: 'lorem ipso',slug: 'hello-world')
+#Article.create(title: 'Hello world',content: 'lorem ipso',slug: 'hello-world')
 #Article.create(title: 'Hello world',content: 'lorem ipso',slug: 'hello-world')
 #User.create(firstname:'Joris',lastname:'Loemba',mail:'jorisloemna@gmail.com')
 #User.create(firstname:'Daniel',lastname:'Kulaya',mail:'getout@gmail.com')
 #User.create(firstname:'Micheal',lastname:'B. Jordan',mail:'killmongera@gmail.com')
 #User.create(firstname:'Zion',lastname:'williamson',mail:'z.williamson@gmail.com')
 #Users.destroy_all
-Users.create(firstname: 'kobe',lastname:'bryant', mail: 'bugs@rubyplus.com', password: '123456')
-Users.create(firstname: 'zion',lastname:'wiil', mail: 'zion@rubyplus.com', password: '123456')
+#Users.create(firstname: 'kobe',lastname:'bryant', mail: 'bugs@rubyplus.com', password: '123456')
+#Users.create(firstname: 'zion',lastname:'wiil', mail: 'zion@rubyplus.com', password: '123456')
+
 #Comment.create(content:'Ce tuto est génial')
 #Comment.create(content:'J\'apprends à coder en ruby ')
 #Comment.create(content:'Python est pas mal aussi')
